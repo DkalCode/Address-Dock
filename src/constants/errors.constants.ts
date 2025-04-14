4eadba845bcccd5c34661453607a83df403742eb
DkalCode <dkalmc@gmail.com>
2025-04-13 23:41:21 -0400

    feat: added google api key & added error constant

--- src/constants/errors.constants.ts
+++ src/constants/errors.constants.ts
@@ -1,2 +1,3 @@
-export const INVALID_REQUEST: string = 'Invalid Request.';
-export const RECORD_NOT_FOUND: string = 'Record not found.';
+export const INVALID_REQUEST: string = "Invalid Request.";
+export const RECORD_NOT_FOUND: string = "Record not found.";
+export const LOCATION_NOT_PROVIDED: string = "Please provide a location.";
