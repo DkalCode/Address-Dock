83846c4d99673acdf5d72ae7706928328c6dd4b4
DkalCode <dkalmc@gmail.com>
2025-04-28 22:36:32 -0400

    fix: refactored to make sense. Checks if values are null or undefined. Used in if statement to validate input.

--- src/utility/validator.utility.ts
+++ src/utility/validator.utility.ts
@@ -2,8 +2,11 @@
 class Validator {
   public constructor() {}
 
-  static isNotNullOrUndefined(value: any[]): boolean {
-    return !value.every((val) => val !== null && val !== undefined);
+  // For every value in the array, check if it is null or undefined.
+  // If all values are null or undefined, return true.
+  // Otherwise, return false.
+  static isNullOrUndefined(value: any[]): boolean {
+    return value.every((val) => val == null || val == undefined);
   }
 }
 
